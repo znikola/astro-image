da70c33fed32053bfe2143c74d174e0200cbfac5
Nikola Zarić <11561723+znikola@users.noreply.github.com>
2023-04-26 23:07:53 +0200

    min repro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: { logLevel: 'info' },
+
+  experimental: {
+    assets: true,
+  },
+  image: {
+    service: 'astro/assets/services/sharp',
+  },
+});
