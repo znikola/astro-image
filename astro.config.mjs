4af63c513d55f7098cdf03f4f6a5de50dde83cb4
Nikola Zarić <11561723+znikola@users.noreply.github.com>
2023-04-27 10:02:47 +0200

    ssr + vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: { logLevel: 'info' },
-
+  vite: {
+    logLevel: 'info',
+  },
   experimental: {
     assets: true,
   },
   image: {
     service: 'astro/assets/services/sharp',
   },
+  output: 'server',
+  adapter: vercel(),
 });
